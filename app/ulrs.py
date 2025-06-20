af5e9e0124a7ff0aadf678e0bb6cd24038225dfe
KuthikBohdan1 <bkuzik14@gmail.com>
2025-06-20 17:34:59 +0300

    fix

--- app/ulrs.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from app.views import site
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('site/', site, name='site'),
-    
-]
