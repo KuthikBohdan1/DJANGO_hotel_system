f9becb0065d5218de2f45eb20b0133b77fad7a0e
KuthikBohdan1 <bkuzik14@gmail.com>
2025-06-21 12:32:10 +0300

    rel

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from app.models import Room
+from .models import Room
 # Create your views here.
 def room_list(request):
     rooms = Room.objects.all()
     context = {
         "all_text": "text",
-        "all_rooms": rooms,
+        "rooms": rooms,
     }
     return render(
         request,
